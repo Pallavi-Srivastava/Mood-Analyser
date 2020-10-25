295cdb9a901245784844f9578a5b392fc8d0266f
Pallavi-Srivastava <69475236+Pallavi-Srivastava@users.noreply.github.com>
2020-10-26 01:49:21 +0530

    Added: Refactor Some Code

--- src/test/java/com/blz/moodanalyzer/ValidateMoodCondition.java
+++ src/test/java/com/blz/moodanalyzer/ValidateMoodCondition.java
@@ -6,16 +6,7 @@ import org.junit.Test;
 public class ValidateMoodCondition {
 
 	static Mood md;
-
-	// Happy mood situation
-	@Test
-	public void testAnalyseMoodCondition_thenAssertionHappy() throws Exception {
-		md = new Mood();
-		String mood = md.analyseAbility("I am in Happy Mood");
-		assertEquals("Happy", mood);
-		System.out.println("Happy");
-	}
-
+	
 	// Sad mood situation
 	@Test
 	public void testAnalyseMoodCondition_thenAssertionSad() throws Exception {
