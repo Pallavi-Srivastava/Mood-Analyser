1ad533e99ab92fd4a5b56519f8b76e18bb2491eb
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-26 03:41:52 +0530

    Added:ExceptionHandle in the Code with verfied Junit TestCase

--- src/main/java/com/blz/moodanalyzer/Mood.java
+++ src/main/java/com/blz/moodanalyzer/Mood.java
@@ -1,21 +1,10 @@
 package com.blz.moodanalyzer;
 
 public class Mood {
-
-	String message;
-
-	public Mood(String message) {
-		super();
-		this.message = message;
-	}
-
-	public Mood() {
-		super();
-	}
-
+	
 	public String analyseAbility(String message) {
-		if (message.contains("I am in Happy Mood"))
-			return "Sad";
-		return "Happy";
+		if (message.contains("Happy"))
+			return "Happy";
+		return "Sad";
 	}
 }
