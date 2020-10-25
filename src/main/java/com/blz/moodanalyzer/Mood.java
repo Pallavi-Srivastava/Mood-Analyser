0eb5b15f10f04cafb1c18a4686c1d554bc1cb541
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-26 02:21:16 +0530

    Added:Refactor the Code

--- src/main/java/com/blz/moodanalyzer/Mood.java
+++ src/main/java/com/blz/moodanalyzer/Mood.java
@@ -1,11 +1,32 @@
 package com.blz.moodanalyzer;
 
+import java.util.Scanner;
+
 public class Mood {
 
-	// Method for mood analysis
-	public String analyseAbility(String msg) throws Exception {
-		if (msg.contains("Happy"))
+	String message;
+
+	public Mood(String message) {
+		super();
+		this.message = message;
+	}
+
+	public Mood() {
+		super();
+	}
+
+	public String analyseAbility() {
+		if (message.contains("Happy"))
 			return "Happy";
 		return "Sad";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter your mood condition");
+		Scanner sc = new Scanner(System.in);
+		Mood m = new Mood();
+		m.message = sc.nextLine();
+		String res = m.analyseAbility();
+		System.out.println(res);
 	}
 }
